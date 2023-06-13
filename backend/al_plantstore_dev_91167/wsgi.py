e2593d9d4c8512426f35cc8e667e0c7cfe0b80b6
Crowdbotics <engineering@crowdbotics.com>
2023-06-13 18:08:33 +0000

    Initial commit.

---
+++ backend/al_plantstore_dev_91167/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for al_plantstore_dev_91167 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'al_plantstore_dev_91167.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "al-plantstore-dev-91167",
+  url: "https://al-plantstore-dev-91167.botics.co",
+  api: "https://al-plantstore-dev-91167.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
