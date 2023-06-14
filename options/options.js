01a628a4ad2cecd828f0c0c4af8364961997f9cb
Aline Tavares <alaisgomes@gmail.com>
2023-06-14 11:21:17 -0400

    add initial Route

--- options/options.js
+++ options/options.js
@@ -6,7 +6,8 @@ export const globalOptions = {
   api: "https://al-plantstore-dev-91167.botics.co/api/v1",
   screenOptions: {
     headerShown: false
-  }
+  },
+  initialScreen: "welcome"
 }
 export const modulesOptions = {
   "@modules/app-menu": {
